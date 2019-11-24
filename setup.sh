5b675afe66bb3673e54b2b7f13f12ad2d1c600c5
Josh <jenninjl01@gmail.com>
2019-11-24 12:18:31 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+# Stuff for setting up a new linux system
+
+# move and reload .bashrc
+mv -f .bashrc ~/
+source ~/.bashrc
+
+# install stuff I like to have
+# cool-retro-term, jakeday surface patch (if using surface), vlc, visual studio code, monodevelop, jdownloader2, firefox, youtube-dl
