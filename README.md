04ce75a998d84437b2f7e1145b2c4f1f22def5f5
Josh <jenninjl01@gmail.com>
2019-11-26 21:37:35 +0000

    updated setup

--- README.md
+++ README.md
@@ -7,17 +7,16 @@ This repo contains useful linux stuff such as installation scripts and my `.bash
 To set up a new linux computer clone this repo, `cd` into it run:
 ```bash
 sudo chmod +x ./setup.sh
-./setup.sh <distro> <surface>
+./setup.sh <surface>
 ```
 
-* `<distro> = ubuntu | arch` is either Ubuntu 18.04 or Arch Linux.
 * `<surface> = surface | <leave empty>` is whether to install the [Jakeday Kernel](https://github.com/jakeday/linux-surface) Patch for the Surface series of computers.
 
 ## Notes: 
 * Not been properly tested so might break.
-* Some thins aren't set up for Arch Linux.
-
 
 ## To Do
 * Move long .bashrc stuff to a bash file. [Tutorial](https://medium.com/devnetwork/how-to-create-your-own-custom-terminal-commands-c5008782a78e).
 * Set up vim.
+* Set up i3 (plus keyboard layer (honestly need to set upi keyboard layout) for i3 stuff).
+* Switch to a better distro than Ubuntu (UBUNTU JUST WORKS SO WELL...) and i3
--- setup.sh
+++ setup.sh
@@ -1,52 +1,11 @@
 # Stuff for setting up a new linux system
 
-# To Do: Need to add stuff for setting up Visual Studio Code extensions (and vim?), add more packages as necessary
+# To Do: Need to add stuff for setting up Visual Studio Code extensions
 
 # Move .bashrc
 mv -f .bashrc ~/
 
-# Install stuff I like to have:
-#
-# cool-retro-term
-# jakeday surface kernel patch (if using surface)
-# vlc
-# vscode
-# monodevelop
-# jdownloader2
-# firefox
-# youtube-dl
-
-if [ "$1" == "ubuntu" ]
-then
-	sudo apt-get update	
-	sudo apt-get install software-properties-common -y
-	
-	# cool-retro-term ppa
-	sudo add-apt-repository ppa:noobslab/apps
-	
-	# MonoDevelop setup
-	sudo apt install apt-transport-https dirmngr -y
-	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-	echo "deb https://download.mono-project.com/repo/ubuntu vs-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-vs.list
-	sudo apt update
-	
-	# Install everything
-	sudo apt-get install ffmpeg
-	sudo apt-get install cool-retro-term vlc code firefox youtube-dl monodevelop -y 
-fi
-
-if [ "$1" == "arch" ]
-then
-	sudo pacman -Syu
-
-	# Install everything
-	sudo pacman -S ffmpeg	
-	sudo pacman -S cool-retro-term vlc code firefox youtube-dl
-
-	echo "MonoDevelop not set up for Arch Linux yet."
-fi
-
-if [ "$1" == "ubuntu" ] & [ "$2" == "surface" ]
+if [ "$1" == "surface" ]
 then
 	# Set up Jakeday kernel patch for surface	
 	sudo apt install git curl wget sed
@@ -55,9 +14,22 @@ then
 	sudo sh setup.sh
 fi
 
-if [ "$1" == "arch" ] & [ "$2" == "surface" ]
-then
-	echo "Jakeday patches not set up for Arch Linux yet."
+# Install stuff I like to have:
+sudo apt-get update	
+sudo apt-get install software-properties-common -y # ppa shit
+
+# cool-retro-term ppa
+sudo add-apt-repository ppa:noobslab/apps
+
+# MonoDevelop setup
+sudo apt install apt-transport-https dirmngr -y
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb https://download.mono-project.com/repo/ubuntu vs-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-vs.list
+sudo apt update
+
+# Install everything
+sudo apt-get install ffmpeg
+sudo apt-get install cool-retro-term vlc code firefox youtube-dl monodevelop htop i3 feh -y 
 fi
 
 # JDownloader2 setup
@@ -66,6 +38,10 @@ wget http://installer.jdownloader.org/JD2SilentSetup_x64.sh
 chmod +x JD2SilentSetup_x64.sh
 ./JD2SilentSetup_x64.sh
 cd ~/
+
+# Set up fallout grub
+wget -O - https://github.com/shvchk/fallout-grub-theme/raw/master/install.sh | bash 
+
 # Reload .bashrc at end because it breaks the first time you run cd
 source ~/.bashrc
 
