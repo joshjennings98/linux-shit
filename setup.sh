087b828815d046d59cc4b39651f5ee0b284a9b79
Josh <jenninjl01@gmail.com>
2020-01-22 20:10:41 +0000

    Added ncdu

--- setup.sh
+++ setup.sh
@@ -59,7 +59,7 @@ xrandr --dpi 180 # Set up dpi scaling for i3
 
 # Install stuff I like to have
 sudo apt-get install network-manager ffmpeg scrot xterm alsamixer vim imagemagick fonts-font-awesome snap -y # Prerequisit/Useful Stuff
-sudo apt-get install vlc firefox youtube-dl htop mc keepass2 veracrypt rdfind pinta spotify -y # Misc Programs
+sudo apt-get install vlc firefox youtube-dl htop mc keepass2 veracrypt rdfind pinta spotify ncdu -y # Misc Programs
 sudo apt-get install nmtui blueman brightnessctl -y # QoL programs
 sudo apt-get install vagrant virtualbox -y # fyp stuff
 snap install code --classic # vscode requires snap
