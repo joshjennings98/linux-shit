d422e584a42d8d86761efdfbe170284c0e402247
Josh <jenninjl01@gmail.com>
2020-04-21 10:51:19 +0100

    Added better power menu when pressing Shift+Mod+e

---
+++ scripts/powermenu.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+rofi_command="rofi -theme ~/linux-stuff/rofi/powermenu.rasi"
+uptime=$(uptime -p | sed -e 's/up //g')
+
+# Options
+lock=" "
+shutdown="-e "
+logout="-e "
+
+# Variable passed to rofi
+options="$lock\n$logout\n$shutdown"
+
+chosen="$(echo -e  "$options" | $rofi_command -p "Uptime - $uptime" -dmenu -selected-row 0)"
+case $chosen in
+    $shutdown)
+        systemctl poweroff
+        ;;
+    $lock)
+        scrot /tmp/screenshot.png ; convert /tmp/screenshot.png -blur 0x8 /tmp/screenshotblur.png ; i3lock -i /tmp/screenshotblur.png
+        ;;
+    $logout)
+        i3-msg exit
+        ;;
+    *)
+	;;
+esac
+
