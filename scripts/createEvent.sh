4c4a9c16cfd6aa4dbaac288128ac895c35f300f0
Josh <jenninjl01@gmail.com>
2020-04-25 22:39:08 +0100

    Fixed missing leading zeros on days less than 10

--- scripts/createEvent.sh
+++ scripts/createEvent.sh
@@ -1,12 +1,19 @@
 # Create an event at the current time on a certain day and month of the current year 
 
 if [ $# -eq 0 ]
+then
+	start=$(date '+%Y%m%dT%H%M00Z')
+	end=$(date -d '-1 hour ago' '+%Y%m%dT%H%M00Z')
+else
+	if [ $1 -gt 10 ]
 	then
-		start=$(date '+%Y%m%dT%H%M00Z')
-		end=$(date -d '-1 hour ago' '+%Y%m%dT%H%M00Z')
-	else
 		start="$(date '+%Y')$2$1$(date '+T%H%M00Z')"
 		end="$(date '+%Y')$2$1$(date -d '-1 hour ago' '+T%H%M00Z')"
+	else
+		x=0 # Need leading zero but if it's just put in it doesn't work
+		start="$(date '+%Y')$2$x$1$(date '+T%H%M00Z')"
+		end="$(date '+%Y')$2$x$1$(date -d '-1 hour ago' '+T%H%M00Z')"
+	fi
 fi
 
 event="https://calendar.google.com/calendar/b/1/r/eventedit?&dates=$start/$end"
