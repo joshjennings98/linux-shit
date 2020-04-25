7df12ae4dd8ccf9a3097de8f3dc08172261e3c26
Josh <jenninjl01@gmail.com>
2020-04-25 10:49:28 +0100

    Made it so when clicking on power, the menu is in the corner

--- scripts/powermenu.sh
+++ scripts/powermenu.sh
@@ -11,7 +11,14 @@ logout="-e  "
 # Variable passed to rofi
 options="$lock\n$logout\n$shutdown"
 
-chosen="$(echo -e  "$options" | $rofi_command -p "Uptime - $uptime" -dmenu -selected-row 0)"
+# If argument then put in corner (for i3status)
+if [ $# -eq 0 ]
+	then
+		chosen="$(echo -e  "$options" | $rofi_command -p "Uptime - $uptime" -dmenu -selected-row 0)"
+	else
+		chosen="$(echo -e  "$options" | $rofi_command -p "Uptime - $uptime" -dmenu -selected-row 0 -location 3 -yoffset 54)"
+fi
+
 case $chosen in
     "-e $lock")
 	scrot /tmp/screenshot.png ; convert /tmp/screenshot.png -blur 0x8 /tmp/screenshotblur.png ; i3lock -i /tmp/screenshotblur.png
