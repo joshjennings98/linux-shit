ad25c118a0b5070b355e5fcca3a5998348b55bd5
Josh <jenninjl01@gmail.com>
2020-05-28 19:03:32 +0100

    Fixed error in cal.sh where startDay always equalled zero. Needed speech marks instead of apostrophes.

--- scripts/cal.sh
+++ scripts/cal.sh
@@ -2,25 +2,25 @@
 
 rofi_command="rofi -theme ~/linux-stuff/rofi/cal.rasi"
 
-if [ "$#" -eq 0 ] ; 
+if [ "$#" -eq 0 ] ;
 then
 	x=`date +'%A %d %B'`
-	hh=`date +%d`
-	m=`date +%m`
-	startday=`date -d '$hh days ago' +%d`
-	day="$(python3 ~/linux-stuff/scripts/cal.py | $rofi_command -p "$x" -dmenu -columns 6 -selected-row $((hh+startday+8)) -location 3 -yoffset 54)"
+	currDay=`date +%d`
+	month=`date +%m`
+	startDay=`date -d "$currDay days ago" +%u`
+	day="$(python3 ~/linux-stuff/scripts/cal.py | $rofi_command -p "$x" -dmenu -columns 6 -selected-row $((currDay+startDay+6)) -location 3 -yoffset 54)"
 else
 	x=`date -d 'next month' +'%B %Y'`
-	hh=`date +%u`
+	currDay=`date +%u`
 	m=`date -d 'next month' +%m`
-	day="$(python3 ~/linux-stuff/scripts/cal.py $1 | $rofi_command -p "$x" -dmenu -columns 6 -selected-row $((hh-1)) -location 3 -yoffset 54)"
+	day="$(python3 ~/linux-stuff/scripts/cal.py $1 | $rofi_command -p "$x" -dmenu -columns 6 -selected-row $((currDay-1)) -location 3 -yoffset 54)"
 fi
 
 if [[ $day =~ [0-9]+ ]] ; then
-	sh ~/linux-stuff/scripts/createEvent.sh $day $m
+	sh ~/linux-stuff/scripts/createEvent.sh $day $month
 else
 	if [[ $day =~ [A-Z]+ ]] ; then
-		date="$(date +'%Y')/$m/$(date +'%d')"
+		date="$(date +'%Y')/$month/$(date +'%d')"
 		addr="https://calendar.google.com/calendar/b/1/r/month/$date"
 		firefox -new-tab $addr
 	fi
---
+++ scripts/files.sh
@@ -0,0 +1,1 @@
+find -type f -printf "%f\n" > $1
