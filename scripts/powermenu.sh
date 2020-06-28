105b2f34edb8d29e00e2ba7c9206ee556a4efd2c
Josh <jenninjl01@gmail.com>
2020-06-28 18:14:03 +0100

    Updated power menu

--- scripts/powermenu.sh
+++ scripts/powermenu.sh
@@ -11,9 +11,9 @@ fi
 uptime=$(uptime -p | sed -e 's/up //g')
 
 # Options
-lock=" "
-shutdown="-e  "
-logout="-e  "
+lock="    Lock"
+shutdown="-e    Shutdown"
+logout="-e    Logout"
 
 # Variable passed to rofi
 options="$lock\n$logout\n$shutdown"
@@ -21,9 +21,9 @@ options="$lock\n$logout\n$shutdown"
 # If argument then put in corner (for i3status)
 if [ $# -eq 0 ]
 	then
-		chosen="$(echo -e  "$options" | $rofi_command -p "Uptime - $uptime" -dmenu -selected-row 0)"
+		chosen="$(echo -e  "$options" | $rofi_command -p "Enceladus - Power" -dmenu -selected-row 0)"
 	else
-		chosen="$(echo -e  "$options" | $rofi_command -p "Uptime - $uptime" -dmenu -selected-row 0 -location 3 -yoffset 54)"
+		chosen="$(echo -e  "$options" | $rofi_command -p "Enceladus - Power" -dmenu -selected-row 0 -location 3 -yoffset 54)"
 fi
 
 case $chosen in
