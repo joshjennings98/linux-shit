e5881d83ebe3c7f57c151b2bfef3079a9d742617
Josh <jenninjl01@gmail.com>
2020-07-01 12:48:32 +0100

    Fixed mistake in setup.sh

--- setup.sh
+++ setup.sh
@@ -205,7 +205,7 @@ then
 	# Polybar
 	mkdir ~/.config/polybar
 	cp ~/linux-stuff/polybar/config ~/.config/polybar
-	cp ~/linux-stuff/scripts/launchPolybar.sh ~/.config/polybar/launch.sh
+	cp ~/linux-stuff/polybar/launch.sh ~/.config/polybar
 
 	# Move ranger stuff
 	mkdir ~/.config/ranger
@@ -245,6 +245,7 @@ then
 	chmod +x ~/linux-stuff/scripts/powermenu.sh
 	chmod +x ~/linux-stuff/scripts/i3-auto-layout
 	chmod +x ~/linux-stuff/scripts/rofiScripts.sh
+	chmod +x ~/linux-stuff/scripts/polybarScripts.sh
 
 	# Reload .bashrc and stuff
 	source ~/.bashrc
