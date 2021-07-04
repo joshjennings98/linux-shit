597a3ce43cd5f3459148f554effef25f9e699f4f
Josh <jenninjl01@gmail.com>
2021-07-04 21:15:11 +0100

    Updated setup.sh comments

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@
 # Currently supports:
 # - Ubuntu
 
-PACKAGES_APT=( # need to work out the packages needed to build stuff later as well as other missing ones
+PACKAGES_APT=(
     "nvidia-driver-465" # may need to update
     "i3"
     "ffmpeg"
@@ -38,7 +38,7 @@ PACKAGES_APT=( # need to work out the packages needed to build stuff later as we
     "curl"
     "mpv"
     "thunar"
-    "w3m" # for ranger (might need ueberzug from pip3?)
+    "w3m" # for ranger
     "xsel"
     "build-essential"
     "libx11-dev"
@@ -69,30 +69,25 @@ cp ~/linux-stuff/Applications/vim/.vimrc ~
 cp ~/linux-stuff/.bashrc ~
 cp ~/linux-stuff/.inputrc ~
 
-# setup slock
+# build and install slock
 cp -r ~/linux-stuff/Applications/slock ~
 cd ~/slock
 sudo make clean install
 rm -rf ~/slock
 
-# setup lutris
-sudo add-apt-repository -y ppa:lutris-team/lutris
-sudo apt update
-sudo apt install -y lutris
-
-# setup dmenu
+# build and install dmenu
 cp -r ~/linux-stuff/Applications/dmenu ~
 cd ~/dmenu
 sudo make clean install
 rm -rf ~/dmenu
 
-# setup st
+# build and install st
 cp -r ~/linux-stuff/Applications/st ~
 cd ~/st
 sudo make install
 rm -rf ~/st
 
-# setup clipmenud/clipnotify
+# build and install clipmenud and clipnotify
 cp -r ~/linux-stuff/Applications/clipnotify ~
 cd ~/clipnotify
 sudo make install
@@ -102,7 +97,12 @@ sudo make install
 rm -rf ~/clipnotify
 rm -rf ~/clipmenu
 
-# setup vscode
+# install lutris
+sudo add-apt-repository -y ppa:lutris-team/lutris
+sudo apt update
+sudo apt install -y lutris
+
+# install and setup vscode
 cd ~
 curl -L -o vscode.deb "https://go.microsoft.com/fwlink/?LinkID=760868"
 sudo apt install -y ./vscode.deb
@@ -112,16 +112,22 @@ cp -r ~/linux-stuff/Applications/vscode/snippets ~/.config/Code/User
 cp -r ~/linux-stuff/Applications/vscode/settings.json ~/.config/Code/User
 ~/Scripts/codeextensions.sh -l ~/linux-stuff/Applications/vscode/vscodeextensions.txt
 
-# setup discord
+# install discord
 cd ~
 curl -L -o discord.deb "https://discord.com/api/download?platform=linux&format=deb"
 sudo apt install -y ./discord.deb
 rm discord.deb
 
-# setup spotify
+# install spotify
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update && sudo apt-get install -y spotify-client
+
+# install minecraft
+cd ~
+curl -L -o minecraft.deb https://launcher.mojang.com/download/Minecraft.deb
+sudo apt install -y ./minecraft.deb
+rm minecraft.deb
 
 # setup fonts
 mkdir -p ~/.local/share/fonts 
@@ -129,7 +135,7 @@ cp ~/linux-stuff/iosevka_regular.ttf ~/.local/share/fonts
 fc-cache -f -v
 
 # reboot pc (optional)
-read -p "Would you like to reboot? (Note: you still need to setup firefox (tablis wallpaper etc.) and probably update  drivers." -n 1 -r
+read -p "Would you like to reboot? (Note: you still need to setup firefox (tablis wallpaper etc.), lutris stuff (including epic games store and origin), and probably update nvidia drivers." -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
